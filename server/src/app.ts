f86ed79ec314b9d69bd3fecc9635ce332c8d218a
Suvini Nimthara <129636274+suvininimthara@users.noreply.github.com>
2024-08-20 13:09:57 +0530

    Add recommendation routes and controller

--- server/src/app.ts
+++ server/src/app.ts
@@ -2,7 +2,8 @@ import "dotenv/config";
 import express, { Request, Response, NextFunction } from "express";
 import UserRoutes from "./routes/userRoutes";
 import bookRoutes from "./routes/bookRoutes";
-
+import recommendationRoutes from "./routes/recommendationRoutes";
+import mongoose from "mongoose";
 
 const app = express();
 
@@ -12,6 +13,7 @@ app.use(express.json());
 // Routes
 app.use('/api/books', bookRoutes);
 app.use('/api/users', UserRoutes);
+app.use('/recommendations', recommendationRoutes);
 
 // Simple logger middleware
 app.use((req: Request, res: Response, next: NextFunction) => {
@@ -27,7 +29,15 @@ app.use((error: unknown, req: Request, res: Response, next: NextFunction) => {
     res.status(500).json({ error: errorMessage });
 });
 
-
+// Connect to MongoDB
+mongoose.connect(process.env.MONGO_CONNECTION_STRING as string)
+    .then(() => {
+        console.log("Connected to MongoDB");
+    })
+    .catch((error) => {
+        console.log("Error connecting to MongoDB", error);
+    });
+    
 // Fallback route for 404
 app.use((req: Request, res: Response) => {
     res.status(404).json({ message: "Route not found" });
