0faab4fbd86d76dec05f219f85474d3819eb7e1c
Suvini Nimthara <129636274+suvininimthara@users.noreply.github.com>
2024-08-21 15:59:34 +0530

    Update dependencies and add Bootstrap for styling

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,10 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Button } from 'react-bootstrap';
 
 function App() {
+  const [clickcount, setCount] = useState(0);
   return (
     <div className="App">
       <header className="App-header">
@@ -10,14 +12,9 @@ function App() {
         <p>
           subscribe to <code>src/App.tsx</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+          <Button onClick={() => setCount(clickcount + 1)}>
+            Click {clickcount} me
+          </Button> 
       </header>
     </div>
   );
