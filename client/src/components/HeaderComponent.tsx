5634f99d2d10cab58d4e9350030f4e689a43af3a
Suvini Nimthara <129636274+suvininimthara@users.noreply.github.com>
2024-08-23 23:06:47 +0530

    chore: Add Bootstrap CSS and update header component

---
+++ client/src/components/HeaderComponent.tsx
@@ -0,0 +1,58 @@
+import React, { useState } from 'react';
+import { Container, Navbar, Nav, Form, FormControl, Button } from 'react-bootstrap';
+import { FaSearch, FaUserCircle } from 'react-icons/fa';
+import './header.css';
+
+const Header: React.FC = () => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false); // Simulate login state
+
+  return (
+    <header>
+      <Navbar expand="lg" className="navbar-custom">
+        <Container>
+          <Navbar.Brand href="/" className="navbar-brand-custom">
+            <img
+              src="logo.png"
+              alt="Logo"
+              style={{ height: '40px', marginRight: '10px' }}
+            />
+            BookRabbit
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link href="/" className="nav-link-custom">Home</Nav.Link>
+              <Nav.Link href="/products" className="nav-link-custom">Books</Nav.Link>
+              <Nav.Link href="/mega-menu" className="nav-link-custom">About</Nav.Link>
+              <Nav.Link href="/contact" className="nav-link-custom">Contact</Nav.Link>
+            </Nav>
+            <Form className="d-flex">
+              <FormControl
+                type="search"
+                placeholder="Search"
+                className="me-2"
+                aria-label="Search"
+              />
+              <Button variant="outline-teal">
+                <FaSearch />
+              </Button>
+            </Form>
+            <Nav>
+              {isLoggedIn ? (
+                <FaUserCircle className="profile-icon" />
+              ) : (
+                <>
+                <p> </p>
+                <Nav.Link href="/" className="nav-link-custom"><Button variant="light" className="ms-2">Login</Button>
+                </Nav.Link> <Nav.Link href="/" className="nav-link-custom"><Button variant="outline-light" className="ms-2">Sign Up</Button>
+                </Nav.Link></>
+              )}
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </header>
+  );
+};
+
+export default Header;
---
+++ client/src/pages/HomePage.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import HeaderComponent from '../components/HeaderComponent';
+
+
+
+const HomePage: React.FC = () => {
+    return (
+        <div>
+            <HeaderComponent />
+            
+        </div>
+    );
+};
+
+export default HomePage;
