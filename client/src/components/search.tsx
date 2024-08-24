aacb45d39777d760a8c67773dffd65f94ff77594
KaweeshaHerath <ar102224@fhss.sjp.ac.lk>
2024-08-24 23:20:34 +0530

    Import Bootstrap CSS

--- client/src/components/search.tsx
+++ client/src/components/search.tsx
@@ -1,7 +1,7 @@
 
 import { useState } from 'react';
 import { FormControl, InputGroup } from 'react-bootstrap'; // Importing Bootstrap components
-
+import 'bootstrap/dist/css/bootstrap.min.css'; // Importing Bootstrap CSS
 
 const Search = () => {
     const [searchTerm, setSearchTerm] = useState('');// Search term entered by user
@@ -12,7 +12,7 @@ const Search = () => {
     return (
         <InputGroup className="mb-3">
             <FormControl
-                placeholder="Search for books"
+                placeholder="Search for books" // Place holder text
                 aria-label="Search"
                 value={searchTerm}
                 onChange={handleSearch}
