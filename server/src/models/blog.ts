71d0b10564d3f1da02a51323c6b826fd0767b0e3
KaweeshaHerath <ar102224@fhss.sjp.ac.lk>
2024-08-27 16:18:00 +0530

    Creating Blog Model

---
+++ server/src/models/blog.ts
@@ -0,0 +1,19 @@
+
+import { Schema, model, Document } from 'mongoose';
+
+export interface Blog extends Document {
+    title: string;
+    content: string;
+    username: string;
+    date: Date;
+}
+
+const blogSchema = new Schema<Blog>({
+    title: { type: String, required: true },
+    content: { type: String, required: true },
+    username: { type: String, required: true },
+    date: { type: Date, default: Date.now },
+});
+
+const Blog = model<Blog>('Blog', blogSchema);
+export default Blog;
