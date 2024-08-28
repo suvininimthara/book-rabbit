13714528a27a9ee35a0e52686712aada001ba8a4
Suvini Nimthara <129636274+suvininimthara@users.noreply.github.com>
2024-08-28 10:31:28 +0530

    Update FeedbackComponent to display user's feedback with a heading

--- client/src/pages/HomePage.tsx
+++ client/src/pages/HomePage.tsx
@@ -1,11 +1,7 @@
 import React from 'react';
 import HeroSection from '../components/HeroSection';
-import BookCardComponent from '../components/BookCardComponent';
-import CategoryCardComponent from '../components/CategoryCardComponent';
-import ContributorCardComponent from '../components/ContributorCardComponent';
 import FeedbackComponent from '../components/FeedbackComponent';
 import BlogSection from '../components/home/BlogSection';
-import { Button } from 'react-bootstrap'; // Import the Button component from the appropriate module
 import RecentBooks from '../components/home/RecentBookSection';
 
 
@@ -21,31 +17,13 @@ const HomePage: React.FC = () => {
             <BlogSection />
             
             </section>
-            <section className="container my-5">
-                <h4>Top Rated</h4>
-                <h6>Top rated books by our users</h6>
-            <div className="book-card-container"></div>
-            <div className='mx-auto text-center'><Button variant="outline-light" className="ms-2" style={{color:'#1bb39e ', border: '1px solid #1bb39e'}}>Browes All</Button>
-            </div>
-            </section>
-            <section className="container my-5">
-                <h2>Top Categories</h2>
-                <div className="d-flex justify-content-around">
-                    <CategoryCardComponent category="Fiction" description="Explore fictional worlds." />
-                    <CategoryCardComponent category="Thriller" description="Learn from reality." />
-                </div>
-            </section>
-            <section className="container my-5">
-                <h2>Top Categories</h2>
-                <div className="d-flex justify-content-around">
-                    <ContributorCardComponent name= "Mr. Saman Perera" contribution= "Author" />
-                    <ContributorCardComponent name= "Mr. Saman Perera" contribution= "Author" />
-                </div>
-            </section>
+            
+            
             <section className="container my-5">
                 <h2>Customer Feedback</h2>
-                <FeedbackComponent feedback="Great platform!" user="User One" />
-                <FeedbackComponent feedback="Amazing book selection!" user="User Two" />
+                <FeedbackComponent feedback="Great platform!" user="sakura" />
+                <FeedbackComponent feedback="Amazing book collection!" user="dilhani" />
+                <FeedbackComponent feedback="I love this site!. The bloging feature is amazing." user="nimesha" />
             </section>
 
 
