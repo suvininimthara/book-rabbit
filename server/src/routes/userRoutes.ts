1073eb3591ada577befc61db56a92858a62ab086
Suvini Nimthara <129636274+suvininimthara@users.noreply.github.com>
2024-08-30 17:26:55 +0530

    Update userRoutes to use more descriptive endpoint for getting all users

--- server/src/routes/userRoutes.ts
+++ server/src/routes/userRoutes.ts
@@ -7,12 +7,14 @@ router.post('/signup', UserController.signUp);
 
 router.post('/login', UserController.login);
 
+router.get('/', UserController.getAuthenticatedUser);
+
 router.post('/logout', UserController.logout);
 
 router.get('/:id', UserController.getUserById);
 
 router.patch('/:id', UserController.updateUser);
 
-router.get('/', UserController.getAllUsers);
+router.get('/all', UserController.getAllUsers);
 
 export default router;
