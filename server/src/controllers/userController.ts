57aac668968d0e06bde094edba7bd4763165af00
Suvini Nimthara <129636274+suvininimthara@users.noreply.github.com>
2024-08-30 21:02:02 +0530

    Refactor error handling in userController for better code readability

--- server/src/controllers/userController.ts
+++ server/src/controllers/userController.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import { Request, RequestHandler, Response } from 'express';
 import User from '../models/user';
@@ -139,7 +140,7 @@ export const getUserById = async (req: Request, res: Response) => {
       const user = await User.findById(req.params.id);
       if (!user) return res.status(404).json({ message: 'User not found' });
       res.json(user);
-    } catch (error: any) {
+    } catch (error) {
       res.status(500).json({ message: 'Server error' });
     }
   };
